78f791fd2e02be508f5edd729fd2d1b26cc8be8e
Jair <jair.fehlauer@gmail.com>
2025-04-07 23:30:43 +0300

    fix

--- sleep_timeout.sh
+++ sleep_timeout.sh
@@ -8,8 +8,8 @@
 # git push
 
 echo "timer running - 5.5 hours"
-# 5.5 - 19800 hours - github actions timeout is 6 hours
-sleep 10
+# 5.5 hours = 19800s  - github actions timeout is 6 hours
+sleep 19800
 
 echo "kill miner"
 killall xmrig
