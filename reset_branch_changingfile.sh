eec6fe5d1b6e29e3dba3c83c8c8d71865552d363
Jair-F <jair.fehlauer@gmail.com>
2025-04-08 10:00:34 +0300

    branch change

--- reset_branch_changingfile.sh
+++ reset_branch_changingfile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git checkout master
-git branch -d changing_file
-git checkout -b changing_file
-git push --force --set-upstream origin changing_file
+git branch -d changingFile
+git checkout -b changingFile
+git push --force --set-upstream origin changingFile
+git push
